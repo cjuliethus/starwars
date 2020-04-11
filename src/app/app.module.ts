69aca063c9343966de85477f6a6185501187505d
Julieth Urquijo <cjuliethus@gmail.com>
2020-04-11 17:31:32 -0500

    added component for details movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DetailsMovieComponent } from './details-movie/details-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsMovieComponent
   ],
   imports: [
     BrowserModule
