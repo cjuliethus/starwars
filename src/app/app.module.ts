cb909bc96ebab04d331d65574e06a58fee94927c
marlenkasafus <marlenkasafus@gmail.com>
2020-04-12 20:19:58 -0500

    se crea componente del botón de busqueda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { DateFormatPipe } from './pipes/date-format.pipe';
 import { DetailsMovieComponent } from './details-movie/details-movie.component';
+import { MovieFilterComponent } from './movie-filter/movie-filter.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { DetailsMovieComponent } from './details-movie/details-movie.component';
     DetailsMovieComponent,
     MovieCardComponent,
     DateFormatPipe,
+    MovieFilterComponent,
   ],
   imports: [BrowserModule],
   providers: [],
