4c69e062c4e8bbfeff20d67cb3d730cd926ee9a8
marlenkasafus <marlenkasafus@gmail.com>
2020-04-13 21:39:13 -0500

    se agrega campo de filtro por nombre

--- src/app/movie-filter/movie-filter.component.ts
+++ src/app/movie-filter/movie-filter.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movie-filter',
   templateUrl: './movie-filter.component.html',
-  styleUrls: ['./movie-filter.component.css']
+  styleUrls: ['./movie-filter.component.less']
 })
 export class MovieFilterComponent implements OnInit {
 
