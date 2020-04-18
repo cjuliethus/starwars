229cf7b0ce185543ea5350b4d5c974c810b0c6cc
santiago.cuervo <sancramirez@gmail.com>
2020-04-17 21:50:22 -0500

    Agrega filtro por anio

--- src/app/movie-filter-criteria/movie-filter-criteria.component.ts
+++ src/app/movie-filter-criteria/movie-filter-criteria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-movie-filter-criteria',
@@ -6,10 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./movie-filter-criteria.component.less']
 })
 export class MovieFilterCriteriaComponent implements OnInit {
-  constructor() { }
+
+  years = Number[200]
+
+  constructor() {
+    this.years = this.getYears()
+  }
 
   @Input()
   open = false;
+
+  getYears() {
+    const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+    return range(1900,2020,1)
+  }
 
   closeFilterCard() {
     this.open = false;
